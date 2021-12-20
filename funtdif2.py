7933a2ac7bc8f21b7a16d835cf0e3ecdc71acb08
Mohd Areeb Fakhar <mrarebb7867@gmail.com>
2021-12-21 00:40:11 +0530

    def2

---
+++ funtdif2.py
@@ -0,0 +1,18 @@
+
+
+def friends(discription):
+    if discription=='anas':
+        print('kind hearted boy')
+    elif discription=='fahim':
+        print('nice person')
+    elif discription=='maheep':
+        print('helping ')
+    else:
+        print('not a friend')
+
+discription=input('enter the name:')
+friends(discription)
+
+
+
+
